2fe94e570792a05b78b5a87c09e5238feaaa4ef4
Aditi Walunj <aditiw.1705@gmail.com>
2025-07-20 00:03:14 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,15 @@
+package Area;
+
+class Circle{
+	static double Pi=3.14;
+	void insert(int radius) {
+		double area=Pi*radius*radius;
+		System.out.println("Area of circle is:"+area);
+	}
+}
+public class AreaOfCircle {
+public static void main(String[] args) {
+	Circle c1=new Circle();
+	c1.insert(2);
+}
+}
