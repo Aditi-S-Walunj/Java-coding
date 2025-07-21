60b50d5eddb2e47090c8b8d1b3fb76b8d2ed9521
Aditi Walunj <aditiw.1705@gmail.com>
2025-07-21 23:06:55 +0530

    Add files via upload

---
+++ Varr.java
@@ -0,0 +1,19 @@
+package var;
+
+public class Varr {
+		static String Name;
+		static int RollNo;
+		static int Age;
+		void display() {
+			System.out.println("Your name is:"+Name);	
+			System.out.println("Your Roll No is:"+RollNo);	
+			System.out.println("Your Age is:"+Age);
+		}
+public static void main(String[] args){
+	Varr v1=new Varr();
+	Varr.Name="Aditi";
+	Varr.RollNo=95;
+	Varr.Age=20;
+	v1.display();
+}
+}
