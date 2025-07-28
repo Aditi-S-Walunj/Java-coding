14a46943c843fc3007e9617f7d426680060c32d7
Aditi Walunj <aditiw.1705@gmail.com>
2025-07-28 22:36:43 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,26 +1,46 @@
-package com.Final;
+package abc;
 
+class OfficialDetails{
+	int EmpId;
+	String EmpName;
+	String EmpDesig;
+	float EmpSal;
+	String EmpDept;
+	boolean EmpStatus;
+void insert(int EmpId,String EmpName,String EmpDesig,float EmpSal,String EmpDept,boolean EmpStatus) 
+    {
+	System.out.println(EmpId);
+	System.out.println(EmpName);	
+	System.out.println(EmpDesig);	
+	System.out.println(EmpSal);	
+	System.out.println(EmpDept);
+	System.out.println(EmpStatus);	
+	}
+void display() {
+}
+}
+class PersonalDetails{
+	long AdharNo;
+	long MobileNo;
+	String PanNo;
+	String Address;
+void insert(long AdharNo,long MobileNo,String PanNo,String Address) 
+    {	
+	System.out.println(AdharNo);	
+	System.out.println(MobileNo);	
+	System.out.println(PanNo);	
+	System.out.println(Address);	
+	}
+void display() {	
+}
+}
 public class Employee {
-	static String CompanyName;
-	int Id;
-	String Name;
-	float salary;
-	static {
-		CompanyName="Future Developers";
-	}
-	Employee(int Id,String Name,float salary){
-		this.Id=Id;
-		this.Name=Name;
-		this.salary=salary;
-	}
-	void display() {
-		System.out.println("Employee Id:"+Id);
-		System.out.println("Employee Name:"+Name);
-		System.out.println("Employee Salary:"+salary);
-		System.out.println("Company Name:"+CompanyName);
-	}
 public static void main(String[] args) {
-	Employee e1=new Employee(1,"Adit",60000f);
-	e1.display();
+	OfficialDetails o1=new OfficialDetails();
+	PersonalDetails p1=new PersonalDetails();
+	o1.insert(101,"aditi","xyz",2000f,"abc",true);
+	p1.insert(1234567123,1234567895,"xyzabc","Sangamner");
+	o1.display();
+	p1.display();
 }
 }
