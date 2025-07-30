1b07c1470ee0d2dd2b45d140dca80635deb9c521
Aditi Walunj <aditiw.1705@gmail.com>
2025-07-30 20:47:52 +0530

    Add files via upload

---
+++ SwitchEx.java
@@ -0,0 +1,20 @@
+package abc;
+import java.util.Scanner;
+
+public class SwitchEx {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number from 1-3:");
+		int num=sc.nextInt();
+		switch(num){
+		case 1:System.out.println("One");
+		break;
+		case 2:System.out.println("Two");
+		break;
+		case 3:System.out.println("Three");
+		break;
+		default:System.out.println("Number not found");
+		}
+sc.close();
+	}
+}
