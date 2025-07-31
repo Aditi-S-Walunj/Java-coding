d6a9dda04d0f23bbb09a0d4436d40ca6c4e17c08
Aditi Walunj <aditiw.1705@gmail.com>
2025-07-31 20:25:07 +0530

    Add files via upload

--- Operations.java
+++ Operations.java
@@ -1,34 +1,27 @@
-package ac.com;
+//Menu-Based Program for Arithmetic Operations Choose an operation (+, -, *, /) and perform it.
+package abc;
 import java.util.Scanner;
 
 public class Operations {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter first number:-");
-		int no1=sc.nextInt();
-		System.out.println("Enter second number:-");
-		int no2=sc.nextInt();
-		System.out.println("First number is:"+no1);
-		System.out.println("Second number is:"+no2);
-		System.out.println("Addition is:"+(no1+no2));
-		System.out.println("Subtraction is:"+(no1-no2));
-		System.out.println("Multiplication is:"+(no1*no2));
-		System.out.println("Division is:"+(no1/no2));
-		float div=sc.nextFloat();
-		System.out.println("Division is:"+div);
-		System.out.println("Remainder is:"+(no1%no2));
-		if(no1>no2 && no1!=no2)
-		{
-			System.out.println("First number is greater and both numbers are different");	
-		}
-		else if(no1<no2 && no1!=no2)
-		{
-			System.out.println("Second number is greater and both numbers are different");
-		}
-		else if(no1==no2)
-		{
-			System.out.println("Both numbers are equal");
-		}
+		System.out.println("Enter number 1:");
+		int a=sc.nextInt();
+		System.out.println("Enter number 2:");
+		int b=sc.nextInt();
+		System.out.println("Enter the opration (+,-,*,/):");
+		String operation=sc.next();
+		switch(operation){
+		case "+":System.out.println("Addition is:"+(a+b));
+		break;
+		case "-":System.out.println("Subtraction is:"+(a-b));
+		break;
+		case "*":System.out.println("Multiplication is:"+(a*b));
+		break;
+		case "/":System.out.println("Divion is:"+(a/b));
+		break;
+		default:System.out.println("Operation not found");
+}
 		sc.close();
 }
 }
