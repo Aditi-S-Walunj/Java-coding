ac072558ddfa02e1c2e38eead67ded29a830b3c9
Aditi Walunj <aditiw.1705@gmail.com>
2025-08-12 20:56:19 +0530

    Add files via upload

---
+++ SumDo.java
@@ -0,0 +1,18 @@
+package abc;
+
+public class SumDo {
+public static void main(String[] args) {
+	int i=1;
+	int sum=0;
+	int n=10;
+	do {
+		System.out.println(i);
+		sum+=i;
+		i++;
+	}
+	while(i<=n);
+	
+	{
+		System.out.println(sum);	}
+}
+}
