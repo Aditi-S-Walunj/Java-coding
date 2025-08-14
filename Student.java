1176540849997c7eb779a5d64631bc71cef15fba
Aditi Walunj <aditiw.1705@gmail.com>
2025-08-14 20:47:34 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,19 +1,58 @@
 package abc;
+import java.util.Scanner;
 
 public class Student {
-	String name;
 	int rollNo;
-	Student(String name,int rollNo){
-	}
-	void display() {
-		System.out.println("Name of the student is:"+name);
-		System.out.println("Roll No of the student is:"+rollNo);
-	}
-	static {
-		System.out.println("I am student of SMBST");
-	}
-public static void main(String[] args) {
-	Student s1=new Student("Aditi",98);
-	s1.display();
+	String name;
+	double percentage;
+    Student(int rollNo, String name, double percentage) {
+    	this.rollNo = rollNo;
+	    this.name = name;
+	    this.percentage = percentage;
+	    }
+    void display() {
+    	System.out.println(rollNo + "\t" + name + "\t" + percentage);
+    	}
+    static void sortStudents(Student[] students) {
+    	for (int i = 0; i < students.length - 1; i++) {
+    		for (int j = i + 1; j < students.length; j++) {
+    			if (students[i].percentage > students[j].percentage) {
+    				Student temp = students[i];
+	                students[i] = students[j];
+	                students[j] = temp;
+	         }
+	     }
+    }
 }
+
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of students: ");
+	int n = sc.nextInt();
+	sc.nextLine(); 
+	Student[] students = new Student[n];
+	for (int i = 0; i < n; i++) {
+		System.out.println("Enter details for student " + (i + 1));
+        System.out.print("Roll No: ");
+	    int roll = sc.nextInt();
+	    sc.nextLine();
+	    System.out.print("Name: ");
+	    String name = sc.nextLine();
+	    System.out.print("Percentage: ");
+	    double per = sc.nextDouble();
+	    students[i] = new Student(roll, name, per);
+	        }
+	System.out.println("\nOriginal Student List:");
+	System.out.println("Roll\tName\tPercentage");
+	for (Student s : students) {
+		s.display();
+		}
+	sortStudents(students);
+	System.out.println("\nSorted Student List (by Percentage):");
+    System.out.println("Roll\tName\tPercentage");
+    for (Student s : students) {
+	     s.display();
+	     }
+    sc.close();
+ }
 }
