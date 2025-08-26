501dba9d99f52eb1f2a872342f6a19068783868d
Aditi Walunj <aditiw.1705@gmail.com>
2025-08-26 20:36:15 +0530

    Add files via upload

---
+++ MultiLevel.java
@@ -0,0 +1,25 @@
+package abc;
+
+class Animal123 {
+	 void eat() {
+	     System.out.println("Animal is eating");
+	 }
+	}
+class Dog extends Animal123 {
+	 void bark() {
+	     System.out.println("Dog is barking");
+	 }
+	}
+class Labrador extends Dog {
+	 void display() {
+	     System.out.println("Labrador is a type of Dog");
+	 }
+	}
+public class MultiLevel {
+	 public static void main(String[] args) {
+	     Labrador labrador = new Labrador();
+	     labrador.eat();   
+	     labrador.bark();  
+	     labrador.display();
+	 }
+}
