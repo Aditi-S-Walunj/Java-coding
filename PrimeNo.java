51e9a27ab9f211732368613b39624f2be2562c4f
Aditi Walunj <aditiw.1705@gmail.com>
2025-08-26 20:37:16 +0530

    Add files via upload

--- PrimeNo.java
+++ PrimeNo.java
@@ -1,30 +1,21 @@
-//check if a Number is Prime Take a number and check if it is prime using if-else
 package abc;
 import java.util.Scanner;
 
 public class PrimeNo {
-public static void main(String[] args){
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter a number:");
-	int num=sc.nextInt();
-	boolean isPrime = true;
- if (num <= 1){
-	   isPrime = false;
-	   } 
-else {
-	 for (int i = 2; i < num; i++){
-	   if (num % i == 0) {
-	       isPrime = false;
-	       break;
-	       }
-	    }
-	 }
-	    if (isPrime){
-	            System.out.println("Number is prime");
-	        } 
-	    else{
-	            System.out.println("Number is not prime");
-	        }
-sc.close();
+public static void main(String[] args) {
+	int i;
+	Scanner Sc=new Scanner(System.in);
+	System.out.println("Enter a Number:");
+	int n=Sc.nextInt();
+	for(i=2;i<n;i++) 
+	if(n%i==0) 
+		break;
+		if(i==n) {
+			System.out.println("Number is prime");
+		}
+		else {
+			System.out.println("Number is not prime");
+		}
+		Sc.close();
 }
 }
