e46088ca57e34fed6b4065eeb8c1e6c9fa4bf8e2
Aditi Walunj <aditiw.1705@gmail.com>
2025-09-18 21:17:20 +0530

    Add files via upload

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,22 +1,23 @@
-//Check if a Given Number is Odd or Even Using Switch Use modulo operation inside switch-case.
 package abc;
 import java.util.Scanner;
 
 public class EvenOdd {
-	public static void main(String[] args){
-			Scanner sc=new Scanner(System.in);
-			System.out.println("Enter a number:");
-			int num=sc.nextInt();
-			switch(num % 2) {
-            case 0:
-                System.out.println("Number is Even");
-                break;
-            case 1:
-                System.out.println("Number is Odd");
-                break;
-            default:
-                System.out.println("Invalid Input");
-        }
-			sc.close();
+ public static void main(String[] args) { 
+	 int sum=0;
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter a Number:");
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++) {
+		System.out.print(i+"=");
+		if(i%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		sum+=i;
 	}
+	}
+	System.out.println("Sum of Odd Numbers :"+sum);
+	sc.close();
+}
 }
