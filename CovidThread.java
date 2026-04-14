f98b40c2fa37285b0938146f066cab8b5ae2b783
Aditi Walunj <aditiw.1705@gmail.com>
2026-04-14 12:16:19 +0530

    Add files via upload

---
+++ CovidThread.java
@@ -0,0 +1,29 @@
+package abc;
+
+class MyThread extends Thread {
+    String text;
+    int n;
+
+    MyThread(String text, int n) {
+        this.text = text;
+        this.n = n;
+    }
+
+    public void run() {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(text);
+        }
+    }
+}
+public class CovidThread {
+	   public static void main(String[] args) {
+
+	        MyThread t1 = new MyThread("COVID19", 10);
+	        MyThread t2 = new MyThread("LOCKDOWN2020", 20);
+	        MyThread t3 = new MyThread("VACCINATED2021", 30);
+
+	        t1.start();
+	        t2.start();
+	        t3.start();
+	    }
+}
